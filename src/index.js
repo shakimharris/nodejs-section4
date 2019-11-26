3eada0ea76fb9bbc1ebd9c64d86c9278fc75797a
Shakim Harris <amenraprod@gmail.com>
2019-11-26 11:45:49 -0500

    read file func

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import "./styles.css";
+const fs = require("fs");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+setTimeout(() => console.log("This is my first timer!"), 0);
+setImmediate(() => console.log("I/O POLLS done"));
+
+fs.readFile("test-file.txt", () => {
+  console.log("I/P FINISHED!");
+});
+
+console.log("Hello from the top level code!");
