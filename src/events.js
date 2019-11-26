011ab4198eb994db493969d40acd8dd03d52c018
Shakim Harris <amenraprod@gmail.com>
2019-11-26 13:52:47 -0500

    added event listeners

--- src/events.js
+++ src/events.js
@@ -1,1 +1,13 @@
 const EventEmitter = require("events");
+
+const myEvent = new EventEmitter();
+
+myEvent.on("newSale", () => {
+  console.log("There was a new sale!");
+});
+
+myEvent.on("newSale", () => {
+  console.log("There was another sale!");
+});
+
+myEvent.emit("newSale");
