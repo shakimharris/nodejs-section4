8f88f547b8cb877c0b30a6ef949270ad75ee8649
Shakim Harris <amenraprod@gmail.com>
2019-11-26 12:45:28 -0500

    updated timeouts

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ setImmediate(() => console.log("I/O POLLS done"));
 
 fs.readFile("test-file.txt", () => {
   console.log("I/P FINISHED!");
+
+  setTimeout(() => console.log("This is my 1.5th timer!"), 0);
+  setTimeout(() => console.log("This is my 3rd timer!"), 3000);
+  setImmediate(() => console.log("I/O POLLS done"));
+  setImmediate(() => console.log("I/O complete!"));
 });
 
 console.log("Hello from the top level code!");
